83f9772c52bfa84159575fd1e3670bdd60f4b7c9
srihan <bala.manukonda@gmail.com>
2017-12-25 12:36:08 -0600

    aaa

--- src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
+++ src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringCoreDevOpsApplication.class)
 @WebAppConfiguration
 public class SpringCoreDevOpsApplicationTests {
